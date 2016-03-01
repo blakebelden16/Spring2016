d4a27fe15ba47c7d7e27454fbadfb77751981164
Blake Belden <blakebelden16@w-delaware.k12.ia.us>
2016-03-01 13:16:28 -0600

    Added initials
    add initials to changes so changes made are known that are mine

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -16,7 +16,7 @@ Aggregated results are stored in tournament.txt
 Unpublished work (c)2013 Project Lead The Way
 CSE Project 1.3.5 Collaborating on a Project
 Draft, Do Not Distribute
-Version 2/29/2016 
+Version 2/29/2016 BB 
 '''
 
 import random
